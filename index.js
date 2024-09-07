2f25420cbae59d1ab0f0cdeb76728baab8ec0d90
max-mahmud <newmahmud24@gmail.com>
2024-09-07 10:42:36 +0600

    update

--- index.js
+++ index.js
@@ -12,11 +12,15 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json()); // Parse incoming JSON requests
 
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes); 
 
-const PORT= process.env.PORT || 5000
+const PORT= process.env.PORT || 4000
 // Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URL)
